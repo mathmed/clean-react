3bf867ea5162c62b8d8075d64387803c59e279f0
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-07-08 17:48:03 -0300

    refactor: make dinamic

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import Spinner from "../spinner/spinner";
 import Styles from "./form-status-styles.scss";
+import Context from "@/presentation/contexts/form/form-context";
 
 const FormStatus: React.FC = () => {
+  const { state } = useContext(Context);
+  const { mainError, isLoading } = state;
   return (
-    <div className={Styles.errorWrap}>
-      <Spinner className={Styles.spinner} />
-      <span className={Styles.error}>Erro</span>
+    <div data-testid="error-wrap" className={Styles.errorWrap}>
+      {isLoading && <Spinner className={Styles.spinner} />}
+      {mainError && <span className={Styles.error}>{mainError}</span>}
     </div>
   );
 };
