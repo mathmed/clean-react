7ab446208bbcd74393c64e7e4ac6e2b15e4110a2
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-07-08 11:27:01 -0300

    feat: add login page

---
+++ src/presentation/pages/login/login.tsx
@@ -0,0 +1,27 @@
+import Styles from "./login-styles.scss";
+import Header from "@/presentation/components/login-header/login-header";
+import Footer from "@/presentation/components/footer/footer";
+import Input from "@/presentation/components/input/input";
+import React from "react";
+import FormStatus from "@/presentation/components/form-status/form-status";
+
+const Login: React.FC = () => {
+  return (
+    <div className={Styles.login}>
+      <Header />
+      <form className={Styles.form}>
+        <h2>Login</h2>
+        <Input type="email" name="email" placeholder="Digite seu email" />
+        <Input type="password" name="password" placeholder="Digite sua senha" />
+        <button className={Styles.submit} type="submit">
+          Entrar
+        </button>
+        <span className={Styles.link}>Criar conta</span>
+        <FormStatus />
+      </form>
+      <Footer />
+    </div>
+  );
+};
+
+export default Login;
